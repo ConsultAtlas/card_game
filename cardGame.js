0b7947c26c01258a487ce26818c1f0bf6d57c9be
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-12 20:53:09 -0400

    started js

---
+++ cardGame.js
@@ -0,0 +1,23 @@
+// lets create a variable that represents a playing card
+
+//var car = {
+// suit: "Hearts".
+// number: 13,
+// color: "red",
+// isFaceDown: true
+//}
+
+// lets build a deck of cards
+
+var deck = [];
+
+for (var i=0; i<52; i++) {
+	var number = i % 13;
+	var car = {
+		suit: "Hearts".
+		number: number+1,
+		color: "red",
+		isFaceDown: true
+    }
+    deck.push(card);
+}
